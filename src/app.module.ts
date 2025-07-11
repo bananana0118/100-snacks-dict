8cd4618c40bfd320d8deadcf0593f07c35d4ee86
bananana0118 <bananana0118@gmail.com>
2025-07-11 10:55:28 +0900

    feat : setting env, dbconfig

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,6 +3,7 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 import dbConfig from './config/db.config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
   imports: [
@@ -12,7 +13,11 @@ import dbConfig from './config/db.config';
       expandVariables: true,
       load: [dbConfig],
     }),
+    TypeOrmModule.forRootAsync({
+      useFactory: process.env.NODE_ENV === 'production' ? dbConfig : dbConfig,
+    }),
   ],
+
   controllers: [AppController],
   providers: [AppService],
 })
--- src/config/db.config.ts
+++ src/config/db.config.ts
@@ -1,10 +1,10 @@
-import path from 'path';
+import * as path from 'path';
 import { PostgresConnectionOptions } from 'typeorm/driver/postgres/PostgresConnectionOptions';
 
 export default (): PostgresConnectionOptions => ({
   port: +(process.env.PORT || 5432),
   url: process.env.URL,
   type: 'postgres',
-  entities: [path.resolve(__dirname, '..') + '**/*.entity{.ts,.js}'],
+  entities: [path.resolve(__dirname, '..') + '/**/*.entity{.ts,.js}'],
   synchronize: true, //production환경에선 제거
 });
