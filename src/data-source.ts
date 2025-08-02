b3e7180a290b068b747d55f9811cfb7168dfaa86
bananana0118 <bananana0118@gmail.com>
2025-08-02 15:03:25 +0900

    [feat]: cli설정을 위한 data-source 업데이트

---
+++ src/data-source.ts
@@ -0,0 +1,13 @@
+// src/data-source.ts
+import 'dotenv/config'; // .env 로드
+import * as path from 'path';
+import { DataSource } from 'typeorm';
+
+export default new DataSource({
+  type: 'postgres',
+  url: process.env.URL, // DATABASE_URL 같은 env
+  port: +(process.env.PORT || 5432),
+  entities: [path.resolve(__dirname, 'entities/*{.ts,.js}')],
+  migrations: [path.resolve(__dirname, 'migrations/*{.ts,.js}')],
+  synchronize: true, // dev에서만 true, prod에선 false
+});
