f9dc12dd55d1034b12f0b27da7d2e420285d9d85
bananana0118 <bananana0118@gmail.com>
2025-08-14 23:21:18 +0900

    [feat]: entity 명시주입

--- src/config/db.config.ts
+++ src/config/db.config.ts
@@ -1,15 +1,31 @@
 import 'dotenv/config';
 import { registerAs } from '@nestjs/config';
-import * as path from 'path';
 import { PostgresConnectionOptions } from 'typeorm/driver/postgres/PostgresConnectionOptions';
+import { Snack } from 'src/entities/snack.entity';
+import { Brand } from 'src/entities/brand.entity';
+import { SnackType } from 'src/entities/snack-type.entity';
+import { Store } from 'src/entities/store.entity';
+import { Taste } from 'src/entities/taste.entity';
+import { SnackReaction } from 'src/entities/snack-reaction.entity';
+import { User } from 'src/entities/user.entity';
+import { Comment } from 'src/entities/comment.entity';
 
 export default registerAs(
   'dbconfig',
   (): PostgresConnectionOptions => ({
-    port: +(process.env.PORT || 5432),
     url: process.env.URL,
     type: 'postgres',
-    entities: [path.resolve(__dirname, '..') + '/**/*.entity{.ts,.js}'],
+    entities: [
+      Snack,
+      SnackType,
+      Brand,
+      Taste,
+      Store,
+      SnackReaction,
+      SnackType,
+      User,
+      Comment,
+    ],
     synchronize: true, //production환경에선 제거
   }),
 );
